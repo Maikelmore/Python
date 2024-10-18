a7e768333f1e72093a7d9260eb64c280a5ef2765
Miguel.Gomez <gomez.mitjavila@gmail.com>
2024-10-18 09:43:08 +0200

    variables

---
+++ Primeros programas/02.variables y datos primitivos.py
@@ -0,0 +1,16 @@
+#
+#
+#Numero : entero,decimal , o complejo
+#cadenas : comas dobles o comas simples 
+#Varibale :
+numero =10 
+print (numero)
+num1, num2 , mensaje =5,14,"hola "
+print(num1)
+print(num2)
+print(mensaje)
+salida= continuar = True 
+print (salida)
+print (continuar)
+salida = False
+print (salida)
