6a06ee5978fad5ca90b49075ec23fafeed60b8d2
Miguel.Gomez <gomez.mitjavila@gmail.com>
2024-10-18 09:33:01 +0200

    Entradas y salidas

---
+++ Primeros programas/01.entradaysalida.py
@@ -0,0 +1,11 @@
+#print ("Viva el rey")
+#print ("viva España")
+#print ("viva el rey "); print ("Viva el orden y la ley")
+#print("fjacpiusdpiasndpouasncdpidjcb<hildfbasuicdbñiasubcasyhi\#dpisupasydfpaisyufbgaspuiybp9ufh8")
+nombre=input ("Dame tu nombre:  ")
+numero= 10 
+altura =183
+print (nombre)
+print (nombre*10)
+print("El número  ")
+print ("Mi nombres {} tengo {} mido {}".format (nombre,numero,altura ))
