4b513d396f8f30378b941072b0e4797519582112
Miguel.Gomez <gomez.mitjavila@gmail.com>
2024-10-18 10:07:41 +0200

    comit final hoy

--- Primeros programas/02.variables y datos primitivos.py
+++ Primeros programas/02.variables y datos primitivos.py
@@ -1,7 +1,7 @@
 #
 #
 #Numero : entero,decimal , o complejo
-#cadenas : comas dobles o comas simples 
+#cadenas : comas dobles o "comas" simples 
 #Varibale :
 numero =10 
 print (numero)
@@ -14,3 +14,22 @@ print (salida)
 print (continuar)
 salida = False
 print (salida)
+print ("Vamos a conocer los tipos de datos de las variables credas")
+print(type(salida))
+print(type(numero))
+help ("keywords")
+'''False               class               from                or
+None                continue            global              pass
+True                def                 if                  raise
+and                 del                 import              return
+as                  elif                in                  try
+assert              else                is                  while
+async               except              lambda              with
+await               finally             nonlocal            yield
+break               for                 not
+'''
+entero = 7 
+decimal = 8,23
+print(decimal ,(type(float)))
+otronum = 0,5e-3 
+print( otronum,(type(otronum)))
